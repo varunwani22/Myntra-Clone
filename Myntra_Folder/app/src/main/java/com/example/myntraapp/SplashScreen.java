398de6ad43abbd0a995184d7fc9f911df537091c
Nishant Rishab <imreprisal@gmail.com>
2021-02-02 21:03:53 +0530

    First Commit

---
+++ Myntra_Folder/app/src/main/java/com/example/myntraapp/SplashScreen.java
@@ -0,0 +1,19 @@
+package com.example.myntraapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SystemClock.sleep(1500);
+        Intent intent = new Intent(this,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+}
